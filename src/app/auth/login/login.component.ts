c086d86117850ced588335181be48bafa5c1d2e0
Patrycja Stępień <patrycja.stepien@capgemini.com>
2020-11-09 11:02:50 +0100

    required validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface LoginData {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email = '';
+  password = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(formValue: LoginData): void {
+    console.log(formValue);
+  }
+
+  logEmail(email: string): void {
+    console.log(email);
   }
 
 }
