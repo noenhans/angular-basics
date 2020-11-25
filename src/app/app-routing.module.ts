9184da0706a5816b1ff3033be0b5f5842758007b
Patrycja Stępień <patrycja.stepien@capgemini.com>
2020-11-25 14:32:45 +0100

    routeing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true, paramsInheritanceStrategy: 'always' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
